31228089a4530f9db309f87b282a8b942b80ee46
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2025-03-18 20:46:37 -0300

    cree el componente card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,17 @@
+import { useContext } from 'react'
 import './Card.scss'
+import CarritoContext from '../contexts/CarritoContext'
 
 const Card = ({producto}) => {
+
+    const {agregarProductoAlCarritoContext} = useContext(CarritoContext)
+    const handleAgregar = (producto) => {
+        agregarProductoAlCarritoContext(producto)
+    }
+
+
+
+
   return (
     <div className="card">
             <article className="card__article">
@@ -11,6 +22,7 @@ const Card = ({producto}) => {
                     <h2 className="card__heading">{producto.nombre}</h2>
                     <div className="card__description">
                         <p>{producto.detalles}</p>
+                        <button onClick={() => handleAgregar(producto)}>Comprar</button>
                     </div>
                 </div>
             </article>
