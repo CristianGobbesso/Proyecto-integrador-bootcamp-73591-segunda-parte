b1edd9880e84ce573e7db06005df54f1f7fd6fc4
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2025-03-19 22:28:56 -0300

    Agregue el useTitulo

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,7 +1,20 @@
+import ListadoCarrito from "../components/ListadoCarrito"
+import useTitulo from "../hooks/useTitulo"
 
 const Carrito = () => {
+
+  useTitulo('Carrito')
+
   return (
-    <div>Carrito</div>
+    <>
+    <section className="section-carrito">
+        <header className="section-carrito__header">
+      <h1>Productos en el carrito</h1>
+        </header>
+        </section>
+      
+      <ListadoCarrito />
+    </>
   )
 }
 
