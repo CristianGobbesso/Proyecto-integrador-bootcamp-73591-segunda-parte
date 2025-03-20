d514c77815a9e2a6f003d01af8e2e2a5524775ac
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2025-03-19 22:32:27 -0300

    Realice toda la logica para manipular los productos

--- src/contexts/ProductosContext.jsx
+++ src/contexts/ProductosContext.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react"
-import { useState } from "react"
+
+import { useEffect, useState } from "react"
 import { createContext } from "react"
 import { peticionesHttp } from "../helpers/peticiones-http"
 
@@ -9,6 +9,7 @@ const ProductosContext = createContext()
 const ProductosProvider = ({ children }) => {
     const url = import.meta.env.VITE_BACKEND_PRODUCTOS
     const [productos, setProductos] = useState(null)
+    const [productoAEditar, setProductoAEditar] = useState(null)
 
     useEffect(() => {
         getAllProductos()
@@ -42,9 +43,47 @@ const ProductosProvider = ({ children }) => {
         }
     }
 
+    const actualizarProductoContext = async (productoAEditar) => {
+       try {
+        const options = {
+            method: 'PUT',
+            headers: {'content-type':'application/json'},
+            body: JSON.stringify(productoAEditar)
+        }
+
+
+        const urlActualizar = url + productoAEditar.id
+        const productoEditado = await peticionesHttp(urlActualizar,options)
+
+        const nuevoEstadoProductos = productos.map(prod => prod.id === productoEditado.id ? productoEditado : prod)
+        setProductos(nuevoEstadoProductos)
+       } catch (error) {
+        console.error('[actualizarProductoContext]', error)
+       }
+    }
+
+    const eliminarProductoContext = async(id) => {
+        try {
+            const urlEliminacion = url + id
+            const options = {
+                method:'DELETE'
+            }
+            const prodEliminado = await peticionesHttp(urlEliminacion, options)
+
+            const nuevoEstadoProductos = productos.filter(prod => prod.id !== id)
+            setProductos(nuevoEstadoProductos)
+        } catch (error) {
+            console.error('[eliminarProductoContext]', error)
+        }
+    }
+
     const data = {
         productos,
         crearProductosContext,
+        actualizarProductoContext,
+        eliminarProductoContext,
+        productoAEditar,
+        setProductoAEditar
     }
 
     return <ProductosContext.Provider value={data}>
