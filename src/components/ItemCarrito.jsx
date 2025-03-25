41d22569330b95f3f1702575c98f93b97736dc89
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2025-03-24 21:44:43 -0300

    Agregue la utilidad para convertir el precio a pesos

--- src/components/ItemCarrito.jsx
+++ src/components/ItemCarrito.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import CarritoContext from '../contexts/CarritoContext'
-
+import convertirAPesos from "../utils/convertirAPesos"
 
 
 const ItemCarrito = ({producto}) => {
@@ -9,21 +9,24 @@ const ItemCarrito = ({producto}) => {
 
     const handleEliminar = (id) => {
         console.log('Eliminando el producto...', id)
-        //eliminarProductoDelCarritoContext(id)
+        eliminarProductoDelCarritoContext(id)
     }
 
   return (
+    <>
     <tr>
         <td >
             <img  src={producto.foto} alt={producto.nombre} width="65px" />
         </td>
         <td>{producto.nombre}</td>
         <td>{producto.cantidad}</td>
-        <td>${calcularSubtotal(producto).toFixed(2)}</td>
+        {/* convertir a pesos */}
+        <td>{convertirAPesos(calcularSubtotal(producto).toFixed(2))}</td> 
         <td>
             <button className='btn-eliminar' onClick={() => handleEliminar(producto.id)}>Eliminar</button>
         </td>
     </tr>
+    </>
   )
 }
 
