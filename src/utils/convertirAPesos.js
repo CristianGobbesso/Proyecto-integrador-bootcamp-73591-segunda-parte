b66d9d306436f9013435b1638b4245847207459b
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2025-03-24 21:43:36 -0300

    cree la carpeta utils

---
+++ src/utils/convertirAPesos.js
@@ -0,0 +1,12 @@
+
+const convertirAPesos = (numero) => {
+
+    const valorTransformado =  new Intl.NumberFormat('es-AR', {
+        style: 'currency',
+        currency: 'ARS',
+    }).format(numero)
+
+    return valorTransformado
+}
+
+export default convertirAPesos
