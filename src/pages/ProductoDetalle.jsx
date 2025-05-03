11119046680f995327193d336d8bcd535bf9e93d
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2025-05-03 20:35:10 -0300

    pruebas para el detalle del producto

--- src/pages/ProductoDetalle.jsx
+++ src/pages/ProductoDetalle.jsx
@@ -8,6 +8,7 @@ const ProductoDetalle = () => {
   const { id } = useParams()
 
   console.log(id)
+  
 
   const [productoDetalle, setProductoDetalle] = useState(null)
 
@@ -16,7 +17,7 @@ const ProductoDetalle = () => {
     getOne(id)
 
   }, [])
-
+ 
   const getOne = async (id) => {
 
 
@@ -53,7 +54,9 @@ const ProductoDetalle = () => {
 
             <section className='section-detalle__producto-Detalle'>
               <h2> {productoDetalle.nombre}</h2>
+              {console.log('Foto del producto:', productoDetalle.foto)}
               <img src={`/${productoDetalle.foto}`} alt={productoDetalle.nombre} />
+              
               <p>Marca: {productoDetalle.marca}</p>
               <p>Categoria: {productoDetalle.categoria}</p>
               <p>Detalles: {productoDetalle.detalles}</p>
